32e2745c55f135923750f5bc9ce7ca2c5df00040
longnguyenthanh5898 <louis.nguyen8022@gmail.com>
2022-04-11 22:04:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+//18h13 11/4/2022
+import {useReducer, useRef} from 'react';
+
+//init state
+const initState = {
+  list: '',
+  lists: []
+}
+
+// action
+const SET_LIST = 'set_list'
+const ADD_LIST = 'add_list'
+const REMOVE_LIST = 'remove_list'
+
+const setList = payload =>{
+  return{
+    type: SET_LIST,
+    payload
+  } 
+}
+const addList = payload =>{
+  return {
+    type: ADD_LIST,
+    payload
+  }
+}
+const removeList = payload =>{
+  return {
+    type: REMOVE_LIST,
+    payload
+  }
+}
+
+// reducer
+const reducer = (state, action) =>{
+  let newState 
+  switch (action.type){
+    case SET_LIST:
+      newState = {
+        ...state,
+        list: action.payload
+      }
+      break;
+    case ADD_LIST:
+      newState = {
+        ...state,
+        lists: [...state.lists, action.payload]
+      }
+      break;
+    case REMOVE_LIST:
+      const currentList = [...state.lists]
+      currentList.splice(action.payload, 1)
+      newState = {
+        ...state,
+        lists: [...currentList]
+      }
+      break;
+  }
+  return newState
+
+}
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState)
+  const {list, lists} = state
+  const inputRef = useRef()
+  const handleAdd = () =>{
+    dispatch(addList(list))
+    dispatch(setList(''))
+    inputRef.current.focus()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding: 30}}>
+      <h3>To Do App</h3>
+      <input 
+        value={list}
+        placeholder='Enter list....'
+        onChange={e =>{ dispatch( setList(e.target.value) ) }}
+        ref={inputRef}
+      />
+      <button onClick={handleAdd}>ADD</button>
+      <ul>{lists.map((list, index) =>(
+        <li key={index}>{list}
+          <span class="remove__list-btn"onClick={() =>dispatch(removeList(index))}>&times;</span>
+        </li>
+      ))}</ul>      
     </div>
   );
 }
 
 export default App;
+
+
